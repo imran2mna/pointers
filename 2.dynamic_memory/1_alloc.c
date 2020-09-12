e277da6dc03ce8c65222b9e12b5630c75a4d3690
imran2mna <imranen08@gmail.com>
2020-09-12 20:23:14 +0530

    furnished dynamic memory notes

--- 2.dynamic_memory/1_alloc.c
+++ 2.dynamic_memory/1_alloc.c
@@ -1,26 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void){
-	//memory allocation through malloc function
+/* 
+====
+NOTE
+====
+Dynamic memory allocation through malloc() function.
+The function returns void *, which should be casted into required data type.
+Due to computer architecture differences better to use sizeof() function.
+*/
 	int* pi = (int*) malloc(sizeof(int));
 	*pi = 5; 
 	printf("*pi : %d\n", *pi);
+/*
+====
+NOTE
+====
+Once the pointer used, it can be released through free() function.
+Then assign the pointer to NULL to avoid dangling pointer.
+*/
 	free(pi);
+	pi = NULL;
 
-	// string memory allocation
-	char* pc = (char*) malloc(6);
+
+/*
+====
+NOTE
+====
+Here we create string in heap using malloc() function.
+Then initialize the memory with 0s.
+*/
 	char word[] = {'L', 'i', 'n', 'u', 'x'};
-	int i;
-	for(i=0; i < 8 ; i++){
-		*(pc + i) = 0; 
-	}
+	printf("word : %s\n", word);
 
+	char* pc = (char*) malloc(6*sizeof(char));
+	memset(pc,0,6); 
+/*	
+	int i;
 	for(i=0; i < 5 ; i++){
 		*(pc + i) = word[i]; 
 	}
-	printf("%s\n", pc);
-	printf("%s\n", word);
+
+====
+NOTE
+====
+Even above commented for() loop can be used to copy the content.
+Otherwise simply use the memcpy() function, which is same.
+*/
+	memcpy(pc, word, 5);
+	printf("pc : %s\n", pc);
 	
 	return 0;
 }
